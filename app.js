8b955e90933094dc1ef80b2673d6dcda123ee308
Julian Gruber <julian@juliangruber.com>
2016-12-14 12:39:26 +0100

    add WIP drag and drop code

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const mount = require('choo/mount')
 const log = require('choo-log')
 const choo = require('choo')
 const css = require('sheetify')
+const fs = require('fs')
 
 css('./public/css/base.css', { global: true })
 
@@ -16,3 +17,18 @@ app.model(require('./models/app')())
 // start
 app.router(['/', mainView])
 mount('body', app.start())
+
+window.ondragover = e => e.preventDefault()
+window.ondrop = e => {
+  e.preventDefault()
+  const folder = e.dataTransfer &&
+    e.dataTransfer.files &&
+    e.dataTransfer.files[0] &&
+    e.dataTransfer.files[0].path
+  if (!folder) return
+  fs.stat(folder, (err, stat) => {
+    if (err) throw err
+    if (!stat.isDirectory()) return
+    console.log({ folder })
+  })
+}
